5069d9ed6cef7e978f6dfddbf162eb1f8e40e4a7
dipu <dipu234dahal@gmail.com>
2022-07-25 13:27:29 +0545

    db config change

--- backend/config/dbConn.js
+++ backend/config/dbConn.js
@@ -2,11 +2,11 @@ const mysql = require('mysql2');
 
 const pool = mysql.createPool({
     connectionLimit: 100,
-    host: 'localhost',
-    port: 3306,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
     user: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
-    database: 'artifeed',
+    database: process.env.DB_NAME,
     debug: false
 });
 
